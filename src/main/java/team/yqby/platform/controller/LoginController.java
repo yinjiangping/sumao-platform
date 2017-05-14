99b0d059afa464a365b711f2d5a9b7213e88e64a
jumping <yinjiangping@bestpay.com.cn>
2017-05-14 18:24:23 +0800

    修改数据库连接地址

--- src/main/java/team/yqby/platform/controller/LoginController.java
+++ src/main/java/team/yqby/platform/controller/LoginController.java
@@ -6,7 +6,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.servlet.ModelAndView;
 import team.yqby.platform.common.constant.SystemConstant;
