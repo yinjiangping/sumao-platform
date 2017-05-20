683ab33380a18c829a1ca511f24ac63773cfbc00
jumping <yinjiangping@bestpay.com.cn>
2017-05-20 17:22:41 +0800

    添加控制器

--- src/main/java/team/yqby/platform/controller/FileUploadController.java
+++ src/main/java/team/yqby/platform/controller/FileUploadController.java
@@ -7,6 +7,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.io.FilenameUtils;
 import org.codehaus.jettison.json.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.multipart.MultipartFile;
@@ -21,6 +22,7 @@ import java.io.PrintWriter;
 import java.util.*;
 
 @Slf4j
+@Controller
 public class FileUploadController {
     @Autowired
     private TFileMapper tFileMapper;
